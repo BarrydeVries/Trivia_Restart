7db116817a728547c54f6802e19ecfbc5723b6b9
barry <barry.de.vries123@gmail.com>
2018-05-16 14:05:39 +0200

    Firebase set up

--- app/src/main/java/com/example/barry/trivia_restart/LoginActivity.java
+++ app/src/main/java/com/example/barry/trivia_restart/LoginActivity.java
@@ -1,9 +1,24 @@
 package com.example.barry.trivia_restart;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    String email;
+    String password;
+    FirebaseUser user;
 
 
 
@@ -12,6 +27,67 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        user = mAuth.getCurrentUser();
+        Intent intent = new Intent(LoginActivity.this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void createClicked (View v) {
+        createUser(email, password);
+    }
+
+    public void loginClicked (View v) {
+        signIn(email, password);
+    }
+
+    public void createUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("user created", "createUserWithEmail:success");
+                        user = mAuth.getCurrentUser();
+                        Intent intent = new Intent(LoginActivity.this, GameActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("something went wrong", "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        user = null;
+                    }
+                }
+            });
+    }
+
+    public void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("signed in", "signInWithEmail:success");
+                        user = mAuth.getCurrentUser();
+                        Intent intent = new Intent(LoginActivity.this, GameActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("something went wrong", "signInWithEmail:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        user = null;
+                    }
+                }
+            });
     }
 }
